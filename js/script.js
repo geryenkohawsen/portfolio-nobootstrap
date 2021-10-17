a6f5372e873a6a1828e65459564e2de98022c106
Zarons <gery.zarons@gmail.com>
2021-10-17 18:47:09 +0900

    FINISH, added typing animation script and scroll-up button

--- js/script.js
+++ js/script.js
@@ -5,6 +5,31 @@ $(document).ready(function(){
         }else {
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 20){
+            $('.scroll-up-btn').addClass("show");
+        }else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    // typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["Student", "Freelancer", "Translator", "Developer", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Student", "Freelancer", "Translator", "Developer", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
     // toggle navbar menu script
